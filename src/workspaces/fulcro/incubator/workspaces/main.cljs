8621f356a6d9a589a2052a2d479302cf8dc729d0
Tony Kay <tony.kay@gmail.com>
2018-10-25 08:25:40 -0700

    demo of possible routing structure

--- src/workspaces/fulcro/incubator/workspaces/main.cljs
+++ src/workspaces/fulcro/incubator/workspaces/main.cljs
@@ -3,6 +3,7 @@
             [fulcro.incubator.workspaces.db-helpers-ws]
             [fulcro.incubator.mutation-interface-ws]
             [fulcro.incubator.pessimistic-mutations-ws]
+            [fulcro.incubator.routing-ws]
             [fulcro.incubator.workspaces.ui.reakit-ws]))
 
 (ws/mount)
